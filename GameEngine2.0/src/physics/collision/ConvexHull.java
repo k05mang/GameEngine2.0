efb18c76fb7a41f3643c80b6a6ceac756f1b9fcc
k05mang <kmango2015@gmail.com>
2016-04-09 17:43:41 -0400

    started work on convex hull generation using quick hull

--- GameEngine2.0/src/physics/collision/ConvexHull.java
+++ GameEngine2.0/src/physics/collision/ConvexHull.java
@@ -1,8 +1,8 @@
 package physics.collision;
 
-import glMath.MatrixUtil;
-import glMath.matrices.Mat4;
+import glMath.VecUtil;
 import glMath.vectors.Vec3;
+import glMath.vectors.Vec4;
 
 import java.util.ArrayList;
 
@@ -21,11 +21,87 @@ public class ConvexHull extends CollisionMesh {
 		//generating, the actual number of faces is generally less than this
 		//which will be adjusted at the end of computation
 		faces = new ArrayList<Triangle>(mesh.getNumFaces());
+		genHull();
 	}
 	
 	public ConvexHull(ConvexHull copy){
 		mesh = copy.mesh;
 		faces = copy.faces;
+	}
+	
+	private void genHull(){
+		//TODO this first step can be eliminated if the min and max points are cached during
+		//mesh generation
+		
+		//iterate over the vertices to find the points that have the max/min for each axis
+		int[] minMax = new int[6];//indices into the mesh for the vertices
+		//order of the indices in this array are min x, max x, min y, max y, min z, max z
+		
+		//iterate over the vertices
+		for(int curVertex = 0; curVertex < mesh.getNumVertices(); curVertex++){
+			//get the position of the current vertex
+			Vec3 pos = mesh.getVertex(curVertex).getPos();
+			
+			//POTENTIALLY NEED TO CHECK FOR THE SAME VERTEX BEING SELECTED FOR MULTIPLE MIN AND MAX, maybe
+			
+			//check for min and max
+			//X min
+			if(pos.x < mesh.getVertex(minMax[0]).getPos().x){
+				minMax[0] = curVertex;
+			}
+
+			//X max
+			if(pos.x > mesh.getVertex(minMax[1]).getPos().x){
+				minMax[1] = curVertex;
+			}
+			
+			//Y min
+			if(pos.y < mesh.getVertex(minMax[2]).getPos().y){
+				minMax[2] = curVertex;
+			}
+
+			//Y max
+			if(pos.y > mesh.getVertex(minMax[3]).getPos().y){
+				minMax[3] = curVertex;
+			}
+			
+			//Z min
+			if(pos.z < mesh.getVertex(minMax[4]).getPos().z){
+				minMax[4] = curVertex;
+			}
+
+			//Z max
+			if(pos.z > mesh.getVertex(minMax[5]).getPos().z){
+				minMax[5] = curVertex;
+			}
+		}
+		
+		//compute the vertex pair that would create the longest edge
+		int startVert, endVert;//vertex indices that make the longest edge
+		float curLongest = 0;//value to keep track of the current edge length of start and end
+		//iterate over each vertex and compute its length with the other vertices and decide if it's the longest
+		for(int curVal = 0; curVal < 6; curVal++){
+			for(int nextVal = curVal+1; nextVal < 6; nextVal++){
+				//compute the length
+				float curLength = VecUtil.subtract(
+						mesh.getVertex(minMax[curVal]).getPos(),//current vertex
+						mesh.getVertex(minMax[nextVal]).getPos()//next vertex
+						).length();
+				//check if the computed length is longer than the previous one
+				if(curLength < curLongest){
+					//if it is then set the tracking variables to the current values
+					curLongest = curLength;
+					startVert = minMax[curVal];
+					endVert = minMax[nextVal];
+				}
+			}
+		}
+		
+		//find the vertex that is farthest from this edge
+		
+		//find the vertex that is farthest from this triangle in the direction of the triangle normal
+		
+		//expand the initial tetrahedra to comprise the convex hull of the mesh
 	}
 
 	@Override
@@ -34,8 +110,9 @@ public class ConvexHull extends CollisionMesh {
 		//this way the vertices don't need to be transformed to test against
 		//the direction vector
 		Vec3 orientedDir = transforms.getOrientation().multVec(direction);
-		
-		return findSupport(orientedDir, faces.get(0).he1);
+
+		//transform the final vertex to reflect the world position of the vertex
+		return (Vec3)transforms.getTransform().multVec(new Vec4(findSupport(orientedDir, faces.get(0).he1),1)).swizzle("xyz");
 	}
 	
 	private Vec3 findSupport(Vec3 direction, HalfEdge startEdge){
@@ -53,17 +130,22 @@ public class ConvexHull extends CollisionMesh {
 			
 			//compare the dot product of the current adjacent vertex with the previous found vertex dot product
 			if(curDotDir > foundDotDir){
-				//since the current vertex dot product is greater than the current one we know it is more in the
+				//since the current vertex dot product is greater than the previous one we know it is more in the
 				//direction of the direction vector than the previously found vertex
 				foundDotDir = curDotDir;
 				foundEdge = curEdge;
 			}
+			//update the current edge to be the next edge that has the next adjacent vertex to the starting vertex
+			curEdge = curEdge.next.opposite;
 		}
 		
-		//if no vertex was then the current vertex is the vertex on the mesh most in the direction
+		//if no vertex was found then the current vertex is the vertex on the mesh most in the direction
 		//of the given vector
-		
-		//transform the final vertex to reflect the world position of the vertex
-		
+		if(foundEdge.equals(startEdge)){
+			return mesh.getVertex(foundEdge.sourceVert).getPos();
+		}else{
+			//recurse using the found vertex as a starting point
+			return findSupport(direction, foundEdge);
+		}
 	}
 }
